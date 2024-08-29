fb32401026213a638dd06830a6f3087fd3b1b64e
PabloJL <jup_jim_7@hotmail.com>
2024-08-29 14:13:21 -0600

    Menu working on both large and smaller devices

--- src/components/NavBar/MenuOverlay.jsx
+++ src/components/NavBar/MenuOverlay.jsx
@@ -1,9 +1,14 @@
 function MenuOverlay({ links }) {
   return (
-    <ul className="flex flex-col py-4 items-center md:hidden ">
+    <ul className="flex flex-col py-4 items-center md:hidden">
       {links.map((link, index) => (
         <li key={index}>
-          <a href={link.path} title={link.title} />
+          <a
+            href={link.path}
+            className="block text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white py-2 pl-3 pr-4"
+          >
+            {link.title}
+          </a>
         </li>
       ))}
     </ul>
--- src/components/NavBar/MobileMenu.jsx
+++ src/components/NavBar/MobileMenu.jsx
@@ -1,44 +1,40 @@
 import { useState } from "react";
 import MenuOverlay from "./MenuOverlay";
 
-const MenuIcon = () => {
-  return (
-    <svg
-      className="w-5 h-5 "
-      aria-hidden="true"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
-      <path
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeWidth="2"
-        d="M5 7h14M5 12h14M5 17h14"
-      />
-    </svg>
-  );
-};
+const MenuIcon = () => (
+  <svg
+    className="w-5 h-5"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeWidth="2"
+      d="M5 7h14M5 12h14M5 17h14"
+    />
+  </svg>
+);
 
-const CloseIcon = () => {
-  return (
-    <svg
-      className="w-5 h-5 "
-      aria-hidden="true"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
-      <path
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M6 18 17.94 6M18 18 6.06 6"
-      />
-    </svg>
-  );
-};
+const CloseIcon = () => (
+  <svg
+    className="w-5 h-5"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M6 18 17.94 6M18 18 6.06 6"
+    />
+  </svg>
+);
 
 const navLinks = [
   {
@@ -55,28 +51,42 @@ const navLinks = [
   },
 ];
 
-export default function MobileMenu() {
+export default function Menu() {
   const [status, setStatus] = useState(false);
+
   return (
-    <div>
-      <div className="mobile-menu block md:hidden">
-        {status ? (
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-6 py-2">
+        <a
+          href={"/"}
+          className=" text-2xl md:text-5xl text-white font-semibold p-5"
+        >
+          Jπ
+        </a>
+        <div className="mobile-menu block md:hidden">
           <button
-            onClick={() => setStatus(false)}
+            onClick={() => setStatus(!status)}
             className="flex items-center px-3 py-2 text-slate-200 border rounded border-slate-200 hover:text-white hover:border-white"
           >
-            <CloseIcon />
+            {status ? <CloseIcon /> : <MenuIcon />}
           </button>
-        ) : (
-          <button
-            onClick={() => setStatus(true)}
-            className="flex items-center px-3 py-2 text-slate-200 border rounded border-slate-200 hover:text-white hover:border-white"
-          >
-            <MenuIcon />
-          </button>
-        )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-10">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <a
+                  href={link.path}
+                  className="block text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white py-2 pl-3 pr-4"
+                >
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       {status ? <MenuOverlay links={navLinks} /> : null}
-    </div>
+    </nav>
   );
 }
