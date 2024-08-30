688109928784d050caf9fd7ee63d2e1e43399146
PabloJL <jup_jim_7@hotmail.com>
2024-08-30 14:25:27 -0600

    Adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import compress from "astro-compress";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), compress()],
   output: "server",
+  adapter: vercel(),
 });
